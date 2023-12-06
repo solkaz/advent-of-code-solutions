b41b92f14e46cc931464544be049f2f426411a39
Jeff Held <jh.software.developer@gmail.com>
2023-12-06 21:12:31 +0100

    Fix warnings in 2023/01-1.py

--- 2023/01/01-1.py
+++ 2023/01/01-1.py
@@ -4,18 +4,15 @@ numbers = re.compile(r"^\d$")
 
 
 def calculate_line(s: str) -> int:
-    first: int | None = None
-    second: int | None = None
+    first: str | None = None
+    second: str | None = None
     for char in s:
         if numbers.match(char):
             if first is None:
                 first = char
             else:
                 second = char
-    if second is None:
-        return int(f"{first}{first}")
-    else:
-        return int(f"{first}{second}")
+    return int(f"{first}{first}") if second is None else int(f"{first}{second}")
 
 
 with open("./input.txt", encoding="utf-8") as my_file:
